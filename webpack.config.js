dfa8950b4dbb7eb0abafb7d467aa70b1e3d2ad37
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-06-15 15:23:41 +0300

    Add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
 function buildConfig(env) {
     return require('./config/webpack.' + env + '.config.js');
